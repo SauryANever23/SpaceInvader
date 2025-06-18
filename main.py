a88455d14ad9db5ac96e02970166dda1d930f94e
SauryANever23 <sauryajha9841@gmail.com>
2025-06-18 15:48:12 +0545

    adding assets - icon for now

--- main.py
+++ main.py
@@ -4,15 +4,23 @@ import sys
 pygame.init()
 
 class BaiscInit():
-
-    def set_screen(lenght, bredth):
+    
+    # Function for Initializing the window
+    def set_screen(lenght: int, bredth: int):
         screen = pygame.display.set_mode((lenght, bredth))
         return screen
+    
+    # Functino for setting the title of the program
+    def set_title(name: str):
+        pygame.display.set_caption(name)
 
 def main():
     # Making a window
     screen = BaiscInit.set_screen(800, 600)
     
+    # Title 
+    BaiscInit.set_title(Space Invader)
+
     # this var determines if the program is running
     running = True
     
