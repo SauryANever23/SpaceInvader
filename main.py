e227a44d5b7e97619ba6fb8e5cdb9176e2b2a897
SauryANever23 <sauryajha9841@gmail.com>
2025-06-18 15:42:16 +0545

    added a clas for basicInitialization and made the game loop, got a window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame 
-
+import sys
 # Initializing pygame 
 pygame.init()
 
@@ -8,3 +8,21 @@ class BaiscInit():
     def set_screen(lenght, bredth):
         screen = pygame.display.set_mode((lenght, bredth))
         return screen
+
+def main():
+    # Making a window
+    screen = BaiscInit.set_screen(800, 600)
+    
+    # this var determines if the program is running
+    running = True
+    
+    # game loop
+    while running:
+        
+        for event in pygame.event.get(): # getting the event from pygame.event.get()
+            if event.type == pygame.QUIT:
+                running = False
+                sys.exit()
+
+if __name__ == '__main__':
+    main()
